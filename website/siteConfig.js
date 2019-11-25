c73a0759273095d244fbdefc41f7b5918f0ff9dd
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-11-25 11:36:40 +0100

    Update site config

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -1,65 +1,35 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
-// See https://docusaurus.io/docs/site-config for all the possible
-// site configuration options.
-
-// List of projects/orgs using your project for the users page.
-const users = [
-  {
-    caption: 'User1',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/undraw_open_source.svg',
-    infoLink: 'https://www.facebook.com',
-    pinned: true,
-  },
-];
-
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
+  title: 'CosmWasm Documentation',
+  tagline: 'Get started with mutli-chain smart contracts!',
 
-  // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
-  // For top-level user or org sites, the organization is still the same.
-  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
-  //   organizationName: 'JoelMarcey'
+  url: "https://docs.cosmwasm.com",
+  baseUrl: "/",
 
-  // For no header links in the top nav bar -> headerLinks: [],
+  cname: 'docs.cosmwasm.com',
+  projectName: 'docs',
+  organizationName: 'cosmwasm',
+
+  // algolia: {
+  //   apiKey: 'xxx',
+  //   indexName: 'xxx',
+  // },
+
   headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
-    {doc: 'doc4', label: 'API'},
-    {page: 'help', label: 'Help'},
-    {blog: true, label: 'Blog'},
+    { href: "https://github.com/confio/cosmwasm", label: "GitHub" },
+    // { search: true },
   ],
 
-  // If you have users set above, you add it here:
-  users,
+  disableHeaderTitle: true,
 
-  /* path to images for header/footer */
   headerIcon: 'img/favicon.ico',
-  footerIcon: 'img/favicon.ico',
+  footerIcon: false,
   favicon: 'img/favicon.ico',
 
-  /* Colors for website */
   colors: {
     primaryColor: '#61512b',
     secondaryColor: '#43381e',
   },
 
-  /* Custom fonts for website */
-  /*
   fonts: {
     myFont: [
       "Times New Roman",
@@ -70,41 +40,29 @@ const siteConfig = {
       "system-ui"
     ]
   },
-  */
 
-  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  copyright: `Copyright © ${new Date().getFullYear()} Confio UO`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'default',
+    theme: 'atom-one-light',
   },
 
-  // Add custom scripts here that would be placed in <script> tags.
   scripts: ['https://buttons.github.io/buttons.js'],
 
-  // On page navigation for the current documentation page.
   onPageNav: 'separate',
-  // No .html extensions for paths.
+
+  docsSideNavCollapsible: true,
+
   cleanUrl: true,
 
   // Open Graph and Twitter card images.
   ogImage: 'img/undraw_online.svg',
   twitterImage: 'img/undraw_tweetstorm.svg',
 
-  // For sites with a sizable amount of content, set collapsible to true.
-  // Expand/collapse the links and subcategories under categories.
-  // docsSideNavCollapsible: true,
-
   // Show documentation's last contributor's name.
-  // enableUpdateBy: true,
-
-  // Show documentation's last update time.
-  // enableUpdateTime: true,
-
-  // You may provide arbitrary config keys to be used as needed by your
-  // template. For example, if you need your repo's URL...
-  //   repoUrl: 'https://github.com/facebook/test-site',
+  enableUpdateBy: true,
+  enableUpdateTime: true,
 };
 
 module.exports = siteConfig;
