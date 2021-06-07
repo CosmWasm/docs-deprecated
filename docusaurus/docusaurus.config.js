65844751b4b8a621b99602fce84beb4e54f1408c
orkunkl <kulceorkun@gmail.com>
2021-06-07 13:00:21 +0300

    Remove unused files; change colors

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -85,6 +85,7 @@ module.exports = {
           // Please change this to your repo.
           editUrl:
             'https://github.com/facebook/docusaurus/edit/master/website/',
+          routeBasePath: '/',
         },
         blog: {
           showReadingTime: true,
