3157d8592fdd217e40ffcbf5bb9faa3ebb322f5a
orkunkl <kulceorkun@gmail.com>
2021-06-09 15:05:33 +0300

    Apply gitignore changes

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/Users/orkunkulce/Workspace/confio/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/orkunkulce/Workspace/confio/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/orkunkulce/Workspace/confio/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/orkunkulce/Workspace/confio/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/orkunkulce/Workspace/confio/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/orkunkulce/Workspace/confio/docs/src/css/custom.css"),
-];
