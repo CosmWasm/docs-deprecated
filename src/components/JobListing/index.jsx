ce1abf403d1813b13c3d401f2c1dfe178078b54e
Arda Nakışçı <anakisci@gmail.com>
2021-11-15 13:16:30 +0300

    Refactor listing component

--- src/components/JobListing/index.jsx
+++ src/components/JobListing/index.jsx
@@ -2,9 +2,19 @@ import React from "react";
 import "../../css/jobListing.scss";
 import Link from "@docusaurus/Link";
 
-const JobListing = ({ image, title, company, description, salary }) => {
+const JobListing = ({
+  singleListing = false,
+  image,
+  title,
+  company,
+  description,
+  salary,
+  link,
+}) => {
   return (
-    <div className="job-listing-container">
+    <div
+      className={`job-listing-container ${singleListing && "single-listing"}`}
+    >
       <div className="job-image">
         <img src={image} alt="job image" />
       </div>
@@ -23,16 +33,30 @@ const JobListing = ({ image, title, company, description, salary }) => {
         </div>
         <div className="description">
           <span className="bold">Description: </span>
-          <span>{description.slice(0, 220)}...</span>
-          <Link
-            to={`job-listings/${company.toLowerCase()}/${title
-              .toLowerCase()
-              .split(" ")
-              .join("-")}`}
-          >
-            see more
-          </Link>
+          {singleListing ? (
+            <span>{description}</span>
+          ) : (
+            <>
+              <span>{description.slice(0, 220)}...</span>
+              <Link
+                to={`job-listings/${company.toLowerCase()}/${title
+                  .toLowerCase()
+                  .split(" ")
+                  .join("-")}`}
+              >
+                see more
+              </Link>
+            </>
+          )}
         </div>
+        {singleListing && (
+          <div className="job-link">
+            <br />
+            <a href={link} target="_blank" rel="noopener noreferrer">
+              Go to the job post
+            </a>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/pages/job-listings/confio/go-developer/index.jsx
+++ src/pages/job-listings/confio/go-developer/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Layout from "@theme/Layout";
 import "../../../../css/jobListing.scss";
 import JOBS from "../../../../../static/job-listings.json";
-import JobDescription from "../../../../components/JobDescription";
+import JobListing from "../../../../components/JobListing";
 
 const JOB = JOBS[0];
 
@@ -10,7 +10,7 @@ const Description = () => {
   return (
     <Layout>
       <div style={{ padding: "20px" }}>
-        <JobDescription
+        <JobListing
           image="https://i.kym-cdn.com/photos/images/newsfeed/001/431/201/40f.png"
           title={JOB.title}
           description={JOB.description.map((line) => {
@@ -24,6 +24,7 @@ const Description = () => {
           company={JOB.company}
           salary={JOB.salary}
           link={JOB.link}
+          singleListing
         />
       </div>
     </Layout>
