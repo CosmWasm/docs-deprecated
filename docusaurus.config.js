5030d3d866c8cd0c9245ed06f7453c1f10fcc837
findolor <anakisci@gmail.com>
2022-05-17 12:53:34 +0300

    Add cw-tokens to config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,6 +59,10 @@ const lastReleasedCWPlusVersion = cwplusVersions[0];
               to: `cw-plus/${lastReleasedCWPlusVersion}/overview`,
               label: 'cw-plus',
             },
+            {
+              to: `cw-tokens/overview`,
+              label: 'cw-tokens',
+            },
           ],
         },
         {
@@ -137,6 +141,10 @@ const lastReleasedCWPlusVersion = cwplusVersions[0];
             {
               label: 'CosmWasm/cw-plus',
               href: 'https://github.com/CosmWasm/cw-plus',
+            },
+            {
+              label: 'CosmWasm/cw-tokens',
+              href: 'https://github.com/CosmWasm/cw-tokens',
             },
             {
               label: 'InterWasm/cw-contracts',
@@ -348,6 +356,16 @@ const lastReleasedCWPlusVersion = cwplusVersions[0];
             path: lastReleasedCWPlusVersion
           }
         }
+      },
+    ],
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'cw-tokens',
+        path: 'cw-tokens',
+        routeBasePath: 'cw-tokens',
+        editUrl: 'https://github.com/InterWasm/docs/edit/main/',
+        sidebarPath: require.resolve('./sidebars/sidebarsCwTokens.js'),
       },
     ],
   ],
